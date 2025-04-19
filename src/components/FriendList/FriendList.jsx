617982d3e57f9588867b4258a7eaa29d9b25435c
Nazar <nazarijankosuk@gmail.com>
2025-04-19 16:01:07 +0300

    second turn to restore vercel problem

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,5 +1,5 @@
 import css from './FriendList.module.css';
-import FriendListItem from './FriendListItem/FriendListItem.jsx';
+import FriendListItem from '../FriendListItem/FriendListItem.jsx';
 
 export default function FriendList({ friends }) {
   return (
