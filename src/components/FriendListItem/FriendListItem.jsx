f7f35631870c0dea414f4988c01680ef57382b2a
Nazar <nazarijankosuk@gmail.com>
2025-04-19 19:07:33 +0300

    third turn to pass a HW)

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,8 +1,4 @@
 import css from './FriendListItem.module.css';
-import { clsx } from 'clsx';
-
-const Online = 'Online';
-const Offline = 'Offline';
 
 export default function FriendListItem({ avatar, name, isOnline }) {
   return (
@@ -14,8 +10,8 @@ export default function FriendListItem({ avatar, name, isOnline }) {
         width="48"
       />
       <p className={css.FriendListItemSubt}>{name}</p>
-      <p className={clsx(isOnline ? Online : Offline)}>
-        {isOnline ? Online : Offline}
+      <p className={css[isOnline ? 'Online' : 'Offline']}>
+        {isOnline ? 'Online' : 'Offline'}
       </p>
     </>
   );
